d9e3af63a904fdfdb378a5334818fbb1aa3d478e
lilianau <84553689+lilianau@users.noreply.github.com>
2022-09-30 13:25:43 -0500

    GalaxyApp

---
+++ Translator/src/main/java/com/fisa/model/Word.java
@@ -0,0 +1,14 @@
+package com.fisa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Word {
+    private String name;
+    private String traslation;
+}
